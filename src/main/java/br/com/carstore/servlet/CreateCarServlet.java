3925765434bfc5e4e09a9518265d3d64377bb11a
Enoque Leal <enoque.fsleal@sp.senac.br>
2026-09-24 21:27:09 -0300

    implementação da conexão com o banco de dados e cadastro do carro

--- src/main/java/br/com/carstore/servlet/CreateCarServlet.java
+++ src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -1,5 +1,8 @@
 package br.com.carstore.servlet;
 
+import br.com.carstore.dao.CarDAO;
+import br.com.carstore.model.Car;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,9 +18,13 @@ public class CreateCarServlet extends HttpServlet {
 
         String name = request.getParameter("name");
 
-        System.out.println("O nome do carro é: " + name);
+        Car car = new Car();
+        car.setName(name);
 
-        request.getRequestDispatcher("index.html").forward(request, resp);
+        CarDAO dao = new CarDAO();
+        dao.createCar(car);
+
+        request.getRequestDispatcher("success.html").forward(request, resp);
 
     }
 
